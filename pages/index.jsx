ff41078786b12579aaaf69bca59b0274d9f8837c
AnghelAna <110469641+AnghelAna@users.noreply.github.com>
2022-11-03 19:58:29 +0200

    Second Push - > Build Part

---
+++ pages/index.jsx
@@ -0,0 +1,10 @@
+
+export default function Home() {
+  return (
+    <div>
+      hello boston <i className="fa-solid fa-house"></i>
+<br />
+  <img src="/images/logo.jpg" alt="Alts" />
+    </div>
+  );
+}
